077801b21d00ef0753751816bdf7fc369e3a7eaa
jesusact <131792824+jesusact@users.noreply.github.com>
2024-12-11 12:19:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,5 @@ import streamlit as st
 
 st.title("Classipet")
 st.write("Clasificador de mascotas. Introduzca los datos de su mascota y le diremos a qué clase pertenece.")
+sr.image("img/mascota_img.jpeg", use_container_width=True)
 
